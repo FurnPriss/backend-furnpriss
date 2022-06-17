407fc2b60966b88c7f9db5409e189a66578e59b8
akhfzz <akhfzz23@gmail.com>
2022-06-17 19:12:39 +0700

    fix

--- price_predict/urls.py
+++ price_predict/urls.py
@@ -5,7 +5,7 @@ app_name='pred'
 
 urlpatterns = [
     path('predict/', pricePredictView.as_view(), name='price'),
-    path('predict/detail/', getProduct.as_view(), name='details'),
+    path('predict/<str:category>/', getProduct.as_view(), name='details'),
     path('products/<str:product_id>/stockin', StockInView.as_view(), name='stockin'),
     path('products/<str:product_id>/stockout', StockOutView.as_view(), name='stockout'),
     path('products/graph/<int:month>', graphStockout.as_view(), name='graph'),
